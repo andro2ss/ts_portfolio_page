c97fda2ac0a57449a71487d6cb127ff561bb534f
andro2ss <a.brzeski@hotmail.com>
2022-04-29 21:47:20 +0200

    created and styled start page

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import "./Start.scss";
 
 function Start({ data }: any) {
+  const el: any = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: data.startText.authorDesc,
+      startDelay: 0,
+      typeSpeed: 80,
+      backSpeed: 20,
+      backDelay: 500,
+      loop: true,
+      showCursor: true,
+      autoInsertCss: true,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="content__container">
       <div className="startPage__box">
         <div className="box__image">
           <img src={data.photos.author} alt="Author selfie" />
         </div>
-        <div>jakies teksty</div>
+        <div className="box__text">
+          <h2>
+            {data.startText.headerTxt}{" "}
+            <span>{data.startText.headerAuthor}</span>
+          </h2>
+          <h3>
+            <span ref={el}></span>
+          </h3>
+          <span>{data.startText.motto}</span>
+        </div>
       </div>
     </div>
   );
